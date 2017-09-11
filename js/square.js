de186fef2fb19475f708432e8c647c8ef8ea1ea2
David Hatten <david.r.hatten@gmail.com>
2017-09-10 23:50:54 -0400

    first pass of squares for carly

---
+++ js/square.js
@@ -0,0 +1,18 @@
+function squarePreview() {
+
+}
+
+function squareUpdatePreview() {
+
+}
+
+function squareDraw(canvas, event) {
+    const xyCoords = getMousePositionInCanvas(canvas, event, getPositionOverrides());
+    const sideLength = parseInt($(`#squareSideLength`).val());
+    setLineWidth();
+
+    history.addHistoryRow(
+        `Square-${Date.now()}`,
+        [drawSquare(canvas, sideLength, xyCoords.x, xyCoords.y)]
+    );
+}
