e6f00cc34335f7586438d0236310550fb1b48102
David Hatten <david@complion.com>
2017-09-15 17:03:35 -0400

    partial implementation of flower of life

--- webpack/components/FlowerOfLife.js
+++ webpack/components/FlowerOfLife.js
@@ -7,21 +7,41 @@ class FlowerOfLife extends Component<Props> {
     }
     drawFlower = () => {
         const innerPetals = [];
+        const angleDegrees = [];
         const petalCount = 6;
         const degrees = 360/petalCount;
         var trackDegrees = 0;
         const jsRotation = 1/petalCount;
-
-        innerPetals.push(<circle {...this.props.config} {...this.props.style} />);
+        console.log(this.props);
+        innerPetals.push(<circle cx={this.props.config.position.x} cy={this.props.config.position.y} r="100" {...this.props.style} />);
 
         // Splitting the degrees, then rotate it by that after
         for (let i = 0; i < petalCount; i++) {
             trackDegrees = trackDegrees + degrees;
-            innerPetals.push(((trackDegrees)/180) + jsRotation);
+            angleDegrees.push(((trackDegrees)/180) + jsRotation);
         }
 
+        let outerPetals = innerPetals.slice();
+        let usedPetals = innerPetals.slice();
+        for (let i = 0; i < this.props.config.iterations; i++) {
+            outerPetals = this.drawPetals(`100`, this.props.config.style, outerPetals, angleDegrees, usedPetals);
+            usedPetals = usedPetals.concat(outerPetals);
+        }
 
-        return (<circle cx="100" cy="100" r="100"/>);
+        console.log(usedPetals);
+
+        return usedPetals;
+    }
+    drawPetal = (radius, style, innerPetals, petalAngles, usedPetals) => {
+        let outerPetals = [];
+
+        for (let i = 0; i < innerPetals.length; i++) {
+            for (let j = 0; j < petalAngles.length; j++) {
+                const sqrX = innerPetals[i].x + radius * Math.cos(Math.PI * petalAngles[j]);
+                const sqrY = innerPetals[i].y + radius * Math.sin(Math.PI * petalAngles[j]);
+
+            }
+        }
     }
     render() {
         return(
