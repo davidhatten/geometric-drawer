0e9de971afa025777f7b86aa3f4499e577c0f205
David Hatten <david@complion.com>
2017-09-22 14:48:18 -0400

    added app abstraction layer for redux

---
+++ webpack/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Title from './Title';
+import Studio from './Studio';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+
+class App extends Component {
+    render() {
+        return (
+            <LocaleProvider locale={enUS}>
+                <div>
+                    <Title />
+                    <Studio />
+                </div>
+            </LocaleProvider>
+        );
+    }
+}
+
+export default App;
--- webpack/entry.js
+++ webpack/entry.js
@@ -1,23 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Title from './components/Title';
-import Studio from './components/Studio';
-import { LocaleProvider } from 'antd';
-import enUS from 'antd/lib/locale-provider/en_US';
+import { Provider } from 'react-redux';
+import App from './components/App';
 import 'antd/dist/antd.css';
 import './style.less';
 
-class App extends Component {
-    render() {
-        return (
-            <LocaleProvider locale={enUS}>
-                <div>
-                    <Title />
-                    <Studio />
-                </div>
-            </LocaleProvider>
-        );
-    }
-}
+const wrapApp = AppComponent =>
+    <Provider>
+        <AppComponent />
+    </Provider>;
 
-render(<App />, document.getElementById(`root`));
+render(wrapApp(App), document.getElementById(`root`));
