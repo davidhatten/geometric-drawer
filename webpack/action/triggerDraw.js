84924e1dc1944922d98d4c428e0dfbb785885bc1
David Hatten <david.r.hatten@gmail.com>
2017-09-26 07:45:42 -0400

    added logic to the triggerDraw action to passthrough state

--- webpack/action/triggerDraw.js
+++ webpack/action/triggerDraw.js
@@ -1,6 +1,18 @@
+import { DRAW_SHAPE } from '../action/drawShape';
+
 export const triggerDraw = (location) => {
     return (dispatch, getState) => {
-        // const state = getState();
         console.log("triggerDraw - state", getState());
+        // Fuck it, just put the whole state in for now
+        const currentState = getState();
+        const shape = currentState.selectShape.selectedShape;
+        const config = currentState.shapeConfig[shape];
+
+        const payload = {
+            shape: shape,
+            config: config,
+            location: location,
+        };
+        dispatch({type: DRAW_SHAPE, payload: payload});
     };
 };;
