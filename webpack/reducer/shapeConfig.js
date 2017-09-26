9d38f097d9dc6fd1e92af372c474048767ea87c5
David Hatten <david.r.hatten@gmail.com>
2017-09-26 07:29:52 -0400

    preserved state on shape return

--- webpack/reducer/shapeConfig.js
+++ webpack/reducer/shapeConfig.js
@@ -2,7 +2,7 @@ import { CHANGE_FOL } from '../action/changeFOLConfig';
 import { FOL_CONFIG } from '../shapeConstants';
 
 const initialState = {
-    [FOL_CONFIG]: {iterations: 2},
+    [FOL_CONFIG]: {iterations: 2, radius: 300},
     LOL: {lol: "lol"},
 };
 
@@ -11,7 +11,7 @@ const shapeConfig = (state = initialState, action) => {
     case CHANGE_FOL:
         console.log("changeFOLConfig", state);
         console.log("changeFOLConfig", action.payload.iterations);
-        return { ...state, [FOL_CONFIG]: {iterations: action.payload.iterations} };
+        return { ...state, [FOL_CONFIG]: {...state[FOL_CONFIG], iterations: action.payload.iterations} };
     default:
         return state;
     }
