78481bb9ae096882de2db2341cf2169cdb8f9fff
David Hatten <david@complion.com>
2017-09-29 11:16:11 -0400

    removed test actions and reducers

--- webpack/actions/consoleWrites.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const WRITE_CONSOLE = `WRITE_CONSOLE`;
-
-export const writeConsole = createAction(WRITE_CONSOLE);
--- webpack/actions/drinkBeer.js
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const DRINK_BEER = `DRINK_BEER`;
-
-export const drinkBeer = createAction(DRINK_BEER);
