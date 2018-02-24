fb5100955e42da5ea40bb714cd6ea89b41f9238a
David Hatten <dhatten@covermymeds.com>
2018-02-23 21:30:57 -0500

    refactor slider input into a full React class before adding error boundary

--- webpack/components/controls/SliderInput.js
+++ webpack/components/controls/SliderInput.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Col, Form, InputNumber, Row, Slider, Tooltip } from "antd";
 const FormItem = Form.Item;
 
-const SliderInput = (props) => (
-    <FormItem label={<Tooltip title={props.description}>{props.name}</Tooltip>}>
-        <Col span={18}>
-            <Slider min={props.min} max={props.max} value={props.value} onChange={props.updateValue}/>
-        </Col>
-        <Col span={1}>
-            {/*Empty spacer*/}
-        </Col>
-        <Col>
-            <Tooltip title={props.description}>
-                <InputNumber size="small" min={props.min} max={props.max} value={props.value} onChange={props.updateValue}/>
-            </Tooltip>
-        </Col>
-    </FormItem>
-);
+const validate = (value) => {
+    if (value === undefined) {
+        return 0;
+    }
+
+    return parseInt(value);
+};
+
+class SliderInput extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <FormItem label={<Tooltip title={this.props.description}>{this.props.name}</Tooltip>}>
+                <Col span={18}>
+                    <Slider min={this.props.min} max={this.props.max} value={this.props.value === `` ? this.props.min : this.props.value}
+                        onChange={this.props.updateValue}/>
+                </Col>
+                <Col span={1}>
+                    {/*Empty spacer*/}
+                </Col>
+                <Col>
+                    <Tooltip title={this.props.description}>
+                        <InputNumber size="small" min={this.props.min} max={this.props.max} value={this.props.value}
+                            formatter={value => value} parser={validate} onChange={this.props.updateValue}/>
+                    </Tooltip>
+                </Col>
+            </FormItem>
+        );
+    }
+}
 
 export default SliderInput;
