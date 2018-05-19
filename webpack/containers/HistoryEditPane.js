2ab57d68f01a9e7d61de9739101002412dcf033e
David Hatten <dhatten@covermymeds.com>
2018-05-19 14:31:56 -0400

    compressing history list, changing buttons to icons

--- webpack/containers/HistoryEditPane.js
+++ webpack/containers/HistoryEditPane.js
@@ -35,7 +35,7 @@ class HistoryEditPane extends Component {
         );
         return (
             <Popover onVisibleChange={this.openOrClose} overlayStyle={{ width: `28%` }} placement={`bottom`} title={shape.name} content={<ContentForm />} trigger={`click`}>
-                <Button>Edit</Button>
+                <Button size={`large`} icon={`edit`} />
             </Popover>
         );
     }
