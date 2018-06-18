132eca4f99444a0f3a2ac7a1c59ce0e7b2f5de56
David Hatten <dhatten@covermymeds.com>
2018-06-18 13:23:57 -0400

    push rounded petal drawing into util module

--- webpack/containers/shapes/AbstractPetal.js
+++ webpack/containers/shapes/AbstractPetal.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {buildPetals, getControlPoints, getPetalTipPoints} from "../../petalUtil";
+import { Component } from 'react';
+import { buildPetals, getIndependentControlPoints, getMirroredControlPoints, getPetalTipPoints } from "../../petalUtil";
 
 export default class AbstractPetal extends Component {
     constructor(props) {
@@ -16,13 +16,19 @@ export default class AbstractPetal extends Component {
     }
 
     singleControlPointForBothPetalArmsAlgorithm() {
+        /*
+        This algorithm describes math for a petal that uses a single control point for both petal arms
+        This means that both petal arm paths will use a single control point
+        Visually, this results in one arc of the petal dramatically bending towards the control point
+        This is useful for creating closed shapes
+         */
         const { rotation: angle, axes, innerRadius, outerRadius, x, y, innerXControl, innerYControl, innerGap, outerGap } = this.props;
         const maxAngle = 360 + angle;
         const angleIncrement = 360/axes;
         const centerPoint = [x, y];
 
         const { innerLeftPoint, innerRightPoint, outerLeftPoint, outerRightPoint } = getPetalTipPoints(x, y, innerRadius, outerRadius, innerGap, outerGap);
-        const { rightPoint: controlPoint } = getControlPoints(innerLeftPoint, innerRightPoint, innerXControl, innerYControl);
+        const { rightPoint: controlPoint } = getMirroredControlPoints(innerLeftPoint, innerRightPoint, innerXControl, innerYControl);
 
         const leftPoints = [innerLeftPoint, outerLeftPoint, controlPoint];
         const rightPoints = [innerRightPoint, outerRightPoint, controlPoint];
@@ -30,14 +36,19 @@ export default class AbstractPetal extends Component {
         return buildPetals(this.drawPetal, angle, angleIncrement, maxAngle, centerPoint, leftPoints, rightPoints);
     }
 
-    singleControlPointForEachPetalArmAlgorithm() {
+    singleControlPointMirroredForEachPetalArmAlgorithm() {
+        /*
+        This algorithm describes math for a petal that uses a single mirrored control point for each petal arm
+        So, both petal arms refer to one of two mirrored control points
+        Visually this results in a set of petals where each has vertical symmetry
+         */
         const { rotation: angle, axes, innerRadius, outerRadius, x, y, innerXControl, innerYControl, innerGap, outerGap } = this.props;
         const maxAngle = 360 + angle;
         const angleIncrement = 360/axes;
         const centerPoint = [x, y];
 
         const { innerLeftPoint, innerRightPoint, outerLeftPoint, outerRightPoint } = getPetalTipPoints(x, y, innerRadius, outerRadius, innerGap, outerGap);
-        const { leftPoint: leftControlPoint, rightPoint: rightControlPoint } = getControlPoints(innerLeftPoint, innerRightPoint, innerXControl, innerYControl);
+        const { leftPoint: leftControlPoint, rightPoint: rightControlPoint } = getMirroredControlPoints(innerLeftPoint, innerRightPoint, innerXControl, innerYControl);
 
         const leftPoints = [innerLeftPoint, outerLeftPoint, leftControlPoint];
         const rightPoints = [innerRightPoint, outerRightPoint, rightControlPoint];
@@ -45,15 +56,40 @@ export default class AbstractPetal extends Component {
         return buildPetals(this.drawPetal, angle, angleIncrement, maxAngle, centerPoint, leftPoints, rightPoints);
     }
 
-    doubleControlPointsForEachPetalArmAlgorithm() {
+    singleControlPointIndependentForEachPetalArmAlgorithm() {
+        const { rotation: angle, axes, innerRadius, outerRadius, x, y, innerXLeftControl, innerXRightControl,
+            innerYLeftControl, innerYRightControl, innerGap, outerGap } = this.props;
+        const maxAngle = 360 + angle;
+        const angleIncrement = 360/axes;
+        const centerPoint = [x, y];
+
+        const { innerLeftPoint, innerRightPoint, outerLeftPoint, outerRightPoint } =
+            getPetalTipPoints(x, y, innerRadius, outerRadius, innerGap, outerGap);
+        const { leftPoint: leftControlPoint, rightPoint: rightControlPoint } =
+            getIndependentControlPoints(innerLeftPoint, innerRightPoint, innerXLeftControl, innerXRightControl, innerYLeftControl, innerYRightControl);
+
+        const leftPoints = [innerLeftPoint, outerLeftPoint, leftControlPoint];
+        const rightPoints = [innerRightPoint, outerRightPoint, rightControlPoint];
+
+        return buildPetals(this.drawPetal, angle, angleIncrement, maxAngle, centerPoint, leftPoints, rightPoints);
+    }
+
+    doubleControlPointsMirroredForEachPetalArmAlgorithm() {
+        /*
+        This algorithm describes math for a petal that uses 2 control points for each petal arm.
+        Both petal arms have 2 control points, one that refers to the inner point and one that refers to the outer point
+        Each pair of control points (inner/outer) is mirrored.
+        So, there is only one set of controls for manipulating the control points (inner/outer x/y)
+        Visually this results in a set of petals with vertical symmetry
+         */
         const { rotation: angle, axes, innerRadius, outerRadius, x, y, innerXControl, innerYControl, outerXControl, outerYControl, innerGap, outerGap } = this.props;
         const maxAngle = 360 + angle;
         const angleIncrement = 360/axes;
         const centerPoint = [x, y];
 
         const { innerLeftPoint, innerRightPoint, outerLeftPoint, outerRightPoint } = getPetalTipPoints(x, y, innerRadius, outerRadius, innerGap, outerGap);
-        const { leftPoint: innerLeftControlPoint, rightPoint: innerRightControlPoint } = getControlPoints(innerLeftPoint, innerRightPoint, innerXControl, innerYControl);
-        const { leftPoint: outerLeftControlPoint, rightPoint: outerRightControlPoint } = getControlPoints(outerLeftPoint, outerRightPoint, outerXControl, outerYControl);
+        const { leftPoint: innerLeftControlPoint, rightPoint: innerRightControlPoint } = getMirroredControlPoints(innerLeftPoint, innerRightPoint, innerXControl, innerYControl);
+        const { leftPoint: outerLeftControlPoint, rightPoint: outerRightControlPoint } = getMirroredControlPoints(outerLeftPoint, outerRightPoint, outerXControl, outerYControl);
 
         const leftPoints = [innerLeftPoint, outerLeftPoint, innerLeftControlPoint, outerLeftControlPoint];
         const rightPoints = [innerRightPoint, outerRightPoint, innerRightControlPoint, outerRightControlPoint];
